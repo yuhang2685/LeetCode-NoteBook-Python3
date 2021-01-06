4165830e024de875444435edff7d5e06c3d8b3d1
YUHANG ZHAO <52333379+yuhang2685@users.noreply.github.com>
2021-01-05 23:59:37 -0500

    Create 0080.py

---
+++ Analysis-Solutions/0080/0080.py
@@ -0,0 +1,42 @@
+# Wrong Answer
+# Input [1,1,1,2,2,3]
+# My output [1,2,2,3]
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        if len(nums) < 1:
+            return 0
+        
+        i = 0
+        j = 1
+        # Wrong: Should be new = True
+        new = False
+        
+        while j < len(nums):
+        
+          # Same:
+          if nums[i] == nums[j]:
+            # See at second time
+            if new == True:
+              i+=1
+              nums[i] = nums[j]
+              j+=1
+              new = False
+            # See at more than second time
+            else:
+              j+=1
+              
+          # Diff:    
+          else:
+            i+=1
+            nums[i] = nums[j]
+            j+=1
+            new = True
+                    
+        return i+1
+       
+# Note Python use Not instead of ! as "negation", but "!=" is allowed.    
